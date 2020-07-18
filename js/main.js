c6b16402bc9181d56719cb0ffa670ca9565dc7a6
Vaterpejekos <slusarev.lg@gmail.com>
2020-07-18 16:33:48 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+; (function () {
+	"use strict";
+	// let menuBtn = document.querySelector('.ba-menu-toggle');
+	// let menu = document.querySelector('.ba-nav');
+
+	// menuBtn.addEventListener('click', () => menu.classList.toggle('open'));
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function () {
+			/* Toggle between adding and removing the "active" class,
+			to highlight the button that controls the panel */
+			this.classList.toggle("active");
+
+			/* Toggle between hiding and showing the active panel */
+			var panel = this.nextElementSibling;
+			if (panel.style.display === "block") {
+				panel.style.display = "none";
+			} else {
+				panel.style.display = "block";
+			}
+		});
+	}
+})();
+
